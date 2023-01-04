da571d97f7bf6cbfa18feca000cffc50b59d7007
James Belnap <james6bcdn@gmail.com>
2023-01-04 11:49:22 -0700

    Fix bug in day 8 part 1

--- day8.py
+++ day8.py
@@ -2,23 +2,29 @@
 
 from inputs import day8 as input
 
+# input = '''092357
+# 164821
+# 237967
+# 986210
+# 674218'''
+
 PART1 = True
 
 rows = input.split('\n')
-rowLen = len(rows[0])
-columnLen = len(rows)
+numRows = len(rows)
+numCols = len(rows[0])
 
-map = [[int(j) for j in rows[i]] for i in rows]
+treeMap = [[int(j) for j in rows[i]] for i in range(numRows)]
 
 def isVisible(row, col):
-  return isVisible_above(row, col) or isVisible_below(row, col) or isVisible_left(row, col) or isVisible_right(row, col)
+  return isVisible_above(row,col) or isVisible_below(row,col) or isVisible_left(row,col) or isVisible_right(row,col)
 
 def isVisible_above(row, col):
   '''
   check all trees to the north for trees that would block the view
   '''
   for a in range(row):
-    if int(rows[a][col]) >= int(rows[row][col]):
+    if treeMap[a][col] >= treeMap[row][col]:
       return False
   return True
 
@@ -26,8 +32,10 @@ def isVisible_below(row, col):
   '''
   check all trees to the south for trees that would block the view
   '''
-  for b in range(row+1, rowLen):
-    if int(rows[b][col]) >= int(rows[row][col]):
+  # print(f'isVisible_below w/ row {row} and col {col}')
+  for b in range(row+1, numRows):
+    # print(f'checking row {b} against row {row}')
+    if treeMap[b][col] >= treeMap[row][col]:
       return False
   return True
 
@@ -36,22 +44,22 @@ def isVisible_left(row, col):
   check all trees to the west for trees that would block the view
   '''
   for c in range(col):
-    if int(rows[row][c]) >= int(rows[row][col]):
+    if treeMap[row][c] >= treeMap[row][col]:
       return False
-  return False
+  return True
 
 def isVisible_right(row, col):
   '''
   check all trees to the east for trees that would block the view
   '''
-  for d in range(col+1, columnLen):
-    if int(rows[row][d]) >= int(rows[row][col]):
+  for d in range(col+1, numCols):
+    if treeMap[row][d] >= treeMap[row][col]:
       return False
   return True
 
-visible = (rowLen) * 2 + (columnLen-2) * 2
-for i in range(1, rowLen-1):
-  for j in range(1, columnLen-1):
+visible = numRows * 2 + (numCols-2) * 2
+for i in range(1, numRows-1):
+  for j in range(1, numCols-1):
     if isVisible(i,j):
       visible += 1
 
